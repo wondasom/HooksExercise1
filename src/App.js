c3a6f38ea52e8f47032e8c0532c0880ce9e43661
Dasom Won <59657188+wondasom@users.noreply.github.com>
2020-04-30 14:40:46 +0200

    add validator

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React, { useState } from "react";
 import "./styles.css";
 
-const useInput = initialValue => {
+const useInput = (initialValue, validator) => {
   const [value, setValue] = useState(initialValue);
   const onChange = event => {
-    console.log(event.target);
+    const {
+      target: { value }
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
   };
   return { value, onChange };
 };
 
 export default function App() {
-  const name = useInput("Mr.");
+  const maxLen = value => !value.includes("@");
+  const name = useInput("Mr.", maxLen);
   return (
     <div className="App">
       <h1>Hello</h1>
